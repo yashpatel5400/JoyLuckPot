a21d068d8550364b2b49012d38041e0b000cde8a
Daphne Yang <daphneyang@Daphnes-MacBook-Pro.local>
2016-02-28 09:07:58 -0800

    commit

--- app/routes/event.js
+++ app/routes/event.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.peekRecord('event', params.event_id);
+  	return Ember.RSVP.hash({
+    	event: this.store.peekRecord('event', params.event_id)
+  	});
   }
 });

5543c90946992e959111223e44251c52573861a0
Zachary Liu <liu.zachary@gmail.com>
2016-02-27 21:36:47 -0800

    Add user photos to event page

--- app/mirage/fixtures/users.js
+++ app/mirage/fixtures/users.js
@@ -2,16 +2,19 @@ export default [
   {
     id: 1,
     name: "Test User 1",
+    photo: "https://scontent.xx.fbcdn.net/hphotos-xtf1/t39.1997-6/11891333_523675484452810_1566085336_n.png",
     attendees: [1, 3]
   },
   {
     id: 2,
     name: "Test User 2",
+    photo: "https://scontent.xx.fbcdn.net/hphotos-xaf1/t39.1997-6/851578_1398251740393009_800041298_n.png",
     attendees: [2, 4]
   },
   {
     id: 3,
     name: "Test User 3",
+    photo: "https://scontent.xx.fbcdn.net/hphotos-xfa1/t39.1997-6/851561_631487310212677_1963526595_n.png",
     attendees: [5]
   }
 ];
--- app/models/user.js
+++ app/models/user.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
+  photo: DS.attr(),
   attendees: DS.hasMany('attendee', {async: true})
 });

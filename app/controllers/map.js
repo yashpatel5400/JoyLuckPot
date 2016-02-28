01899549cb68c24c4d2c1de11b6b0776115437b8
Zachary Liu <liu.zachary@gmail.com>
2016-02-28 06:13:29 -0800

    Center on current location when loading map

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   userLocation: null,
+  userLat: 0,
+  userLng: 0,
   onLocationUpdate: function () {
     var loc = this.get("userLocation");
+    this.set("userLat", loc.coords.latitude);
+    this.set("userLng", loc.coords.longitude);
 
     $('.map-location-overlay h1').html('<i class="fa fa-thumbs-o-up"></i>');
     $('.map-location-overlay').addClass('approved');
